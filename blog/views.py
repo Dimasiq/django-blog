a106713108e24a305fdb4b41f721cfaa7ce29468
Dimasiq <otake0447@gmail.com>
2018-10-06 21:04:49 +1000

    'commit'

---
+++ blog/views.py
@@ -0,0 +1,90 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.core.paginator import Paginator
+from django.views.generic import View
+from django.urls import reverse
+from django.db.models import Q
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .models import Post, Tag
+from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixin, ObjectDeleteMixin
+from .forms import TagForm, PostForm
+
+def posts_list(request):
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        posts = Post.objects.filter(Q(title__icontains=search_query) | Q(body__icontains=search_query))
+    else:
+        posts = Post.objects.all()
+
+    paginator = Paginator(posts, 5)
+
+    page_num = request.GET.get('page', 1)
+    page = paginator.get_page(page_num)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url= ''
+
+    context={
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'prev_url': prev_url,
+        'next_url': next_url
+        }
+
+    return render(request, 'blog/index.html', context=context)    
+
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
+
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    model_form = TagForm
+    template = 'blog/tag_create.html'
+    raise_exception = True
+
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
+    model_form = PostForm
+    template = 'blog/post_create.html'
+    raise_exception = True
+
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update.html'
+    raise_exception = True
+
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update.html'
+    raise_exception = True
+
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin ,View):
+    model = Tag
+    template = 'blog/tag_delete.html'
+    redirect_url = 'tags_list_url'
+    raise_exception = True
+
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin ,View):
+    model = Post
+    template = 'blog/post_delete.html'
+    redirect_url = 'posts_list_url'
+    raise_exception = True
